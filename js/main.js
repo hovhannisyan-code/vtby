5273f26a7d8c24c59e00b49fc2b0c8905b7ae023
remox112358 <remox112358@gmail.com>
2020-09-20 17:36:57 +0400

    Add language dropdown and some fix

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 jQuery(document).ready($ => {
 
     $('#menu-button').click(function() {
+        $('.header__buttons button:not(.language-button)').removeClass('active');
+        $('[data-type="dropdown"]').removeClass('show');
+
         $(this).toggleClass('active');
         $('#menu').toggleClass('show');
+    });
+
+    $('#language-button').click(function() {
+        $('.header__buttons button:not(.language-button)').removeClass('active');
+        $('[data-type="dropdown"]').removeClass('show');
+
+        $(this).toggleClass('active');
+        $('#language-list').toggleClass('show');
     });
 
     $('.route').click(function() {
