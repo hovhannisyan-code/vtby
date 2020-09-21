747a1b63eee3fdb31760c18b411ffd61d7843b03
remox112358 <remox112358@gmail.com>
2020-09-21 16:13:38 +0400

    Refactoring: Scripts

--- js/main.js
+++ js/main.js
@@ -1,63 +1,49 @@
 jQuery(document).ready($ => {
 
-    // Menu open
-    $('#menu-button').click(function() {
-        $('.header__buttons button').not('.language-button').removeClass('active');
-        $('[data-type="dropdown"]').removeClass('show');
+    // Remove header active and show classes for other buttons and dropdowns
+    const resetHeader = (button, dropdown) => {
+        $('header .header__buttons [data-type="dropdown-button"]').not(button).removeClass('active');
+        $('header [data-type="dropdown"]').not(dropdown).removeClass('show');
+    };
 
-        $(this).toggleClass('active');
-        $('#menu').toggleClass('show');
-    });
-
-    // Language list open
-    $('#language-button').click(function() {
-        $('.header__buttons button').not('.language-button').removeClass('active');
-        $('[data-type="dropdown"]').removeClass('show');
-
-        $(this).toggleClass('active');
-        $('#language-list').toggleClass('show');
-    });
-
-    // Route open
-    $('.route').click(function() {
-        $(this).toggleClass('active');
-    });
-
-    // Ticket form dropdown open
-    $('.ticket-form__input').click(function() {
-        $(this).siblings('.item__dropdown').addClass('active');
+    // On header dropdown button click
+    $(document).on('click', '[data-type="dropdown-button"]', function() {
+        $button = $(this);
+        $dropdown = $(`${$($button).attr('data-href')}`);
         
+        resetHeader($button, $dropdown);
+
+        $button.toggleClass('active');
+        $dropdown.toggleClass('show');
+    });
+
+    // Route open/close
+    $(document).on('click', '.route', function() {
+        $(this).toggleClass('active');
+    });
+
+    // Ticket form dropdown open/close
+    $(document).on('click', '.item--dropdown', function() {
+        $item = $(this);
+        $dropdown = $item.children('.item__dropdown');
+
+        $item.addClass('active');
+        $dropdown.addClass('active');
+
         // On document click
         $(document).mouseup((event) => {
-            let $dropdown = $('.item__dropdown');
             
             // Ticket form dropdown close
             if (! $dropdown.is(event.target)
                 && $dropdown.has(event.target).length === 0) {
                     $dropdown.removeClass('active');
+                    $item.removeClass('active');
             }
         });
     });
 
-    // Ticket form select dropdown open
-    $('.item--dropdown').click(function() {
-        $(this).addClass('active');
-        $(this).children('.item__dropdown').addClass('active');
-        
-        // On document click
-        $(document).mouseup((event) => {
-            let $dropdown = $('.item__dropdown');
-            
-            // Ticket form dropdown close
-            if (! $dropdown.is(event.target)
-                && $dropdown.has(event.target).length === 0) {
-                    $dropdown.removeClass('active');
-                    $(this).removeClass('active');
-            }
-        });
-    });
-
-    $('[data-type="calendar-button"]').click(function() {
+    // On ticket for date dropdown button click
+    $(document).on('click', '[data-type="calendar-button"]', function() {
         $('[data-type="calendar-button"]').removeClass('active');
         $(this).addClass('active');
     });
